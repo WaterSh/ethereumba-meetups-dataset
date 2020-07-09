16399a4afa0d4dc050ea2766f69c94ad812da9bc
Alan Verbner <alverbner@gmail.com>
2020-07-09 15:56:28 -0300

    refactor: removed an extra file iteration when anonymizing data
    Instead of processing the users first and then the data, both are done
    in the same loop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sqlite3
 from db import create_schema, create_users_mapping_schema
-from anonymize_data import import_users
 from import_data import import_meetup_data, import_meetups
 
 meetups_users = "./import/meetup_users.csv"
@@ -15,12 +14,6 @@ create_users_mapping_schema(anon_db)
 # Process meetups details
 dataset_db = sqlite3.connect('./dataset/ethereumba-dataset.db')
 create_schema(dataset_db)
-
-# TODO: This file will be processed twice but a refactor might be expected when
-# cmd arguments get implemented so no need to worry about it now (also
-# the input file is quite small)
-print("[main] Running data anonymization")
-import_users(anon_db, meetups_users)
 
 print("[main] Importing meetups list")
 import_meetups(meetups_file, dataset_db)
