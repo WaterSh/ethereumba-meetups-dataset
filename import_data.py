2034ec272a3fdabd8f962d81b81c9779c646bbf4
Alan Verbner <alverbner@gmail.com>
2020-07-09 15:56:28 -0300

    refactor: removed an extra file iteration when anonymizing data
    Instead of processing the users first and then the data, both are done
    in the same loop

--- import_data.py
+++ import_data.py
@@ -1,3 +1,4 @@
+from random import SystemRandom
 import sqlite3
 import csv
 
@@ -20,6 +21,27 @@ def import_meetups(path, dataset_db):
     dataset_db.commit()
 
 
+def import_user(anon_db, meetup_user_id):
+    """
+    This function reads Meetup dump information and creates a mapping between a real
+    user_id and our id. The mapping will be used to keep a _user identity_ 
+    without revealing his real data allowing metrics like churn rate to be calculated.
+    """
+    random = SystemRandom()
+    print("[import_user] Procesing {}".format(meetup_user_id))
+    ethereumba_meetup_id = db.get_user_mapping(anon_db, meetup_user_id)
+    if ethereumba_meetup_id is None:
+        new_ethereumba_meetup_id = random.randint(0, 2**32)
+        print("[import_user] User not found. Assigning {} to {}".format(
+            new_ethereumba_meetup_id, meetup_user_id))
+        db.insert_userid_mapping(
+            anon_db, meetup_user_id, new_ethereumba_meetup_id)
+        return new_ethereumba_meetup_id
+    else:
+        print("[import_user] User found")
+        return ethereumba_meetup_id[0]
+
+
 def import_meetup_data(path, dataset_db, anon_db):
     """
     Imports Meetup dump into the dataset using anonymized names
@@ -31,11 +53,10 @@ def import_meetup_data(path, dataset_db, anon_db):
         for row in csv_reader:
             meetup_name = row[0]
             meetup_user_id = row[2]
-            rsvp = row[7] != '' # TODO: We can parse the date here
+            rsvp = row[7] != ''  # TODO: We can parse the date here
             attended = row[10] == 'x'
             print("[import_meetup_data] Processing row {}".format(row))
-            # TODO: This assumes users were processed first and there are no new ones on this file but we should parse them
-            ethereumba_user_id = db.get_user_mapping(anon_db, meetup_user_id)
+            ethereumba_user_id = import_user(anon_db, meetup_user_id)
             print("[import_meetup_data] ethereumba user found {}".format(
                 ethereumba_user_id))
             meetup_id = db.look_for_meetup(cursor, meetup_name)
@@ -43,6 +64,6 @@ def import_meetup_data(path, dataset_db, anon_db):
                 print("[import_meetup_data] Inserting meetup_id: {}, ethereumba_user_id: {}, rsvp: {}, attended: {}".format(
                     meetup_id, ethereumba_user_id, rsvp, attended))
                 db.insert_meetup_data(
-                    cursor, meetup_id[0], ethereumba_user_id[0], rsvp, attended)
+                    cursor, meetup_id[0], ethereumba_user_id, rsvp, attended)
     cursor.close()
     dataset_db.commit()
--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import sqlite3
 from db import create_schema, create_users_mapping_schema
-from anonymize_data import import_users
 from import_data import import_meetup_data, import_meetups
 
-meetups_users = "./import/meetup_users.csv"
 meetups_file = "./import/meetups_list.csv"
 meetup_export = "./import/meetup_dump.csv"
 attendees_without_rsvp = "./import/attendees_without_rsvp.csv"
@@ -15,12 +13,6 @@ create_users_mapping_schema(anon_db)
 # Process meetups details
 dataset_db = sqlite3.connect('./dataset/ethereumba-dataset.db')
 create_schema(dataset_db)
-
-# TODO: This file will be processed twice but a refactor might be expected when
-# cmd arguments get implemented so no need to worry about it now (also
-# the input file is quite small)
-print("[main] Running data anonymization")
-import_users(anon_db, meetups_users)
 
 print("[main] Importing meetups list")
 import_meetups(meetups_file, dataset_db)
